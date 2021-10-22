623fb8cc54d7cee1444b42cf4ceb8e296000a0a4
Lucaspadularrosa <lucas_sfc98@outlook.com>
2021-10-22 15:55:45 -0300

    corrección de errores nucba

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Projects from './components/Projects';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import logo from './img/foto-logo.jpg';
-import { TiCode } from 'react-icons/ti';
+import { AiOutlineAlignLeft } from 'react-icons/ai';
 
 function App() {
   const logoImg = useRef();
@@ -14,54 +14,58 @@ function App() {
     const menu = logoImg.current;
     menu.classList.toggle('invisible');
   };
+
   return (
     <div className='w-full bg-black text-white'>
       <BrowserRouter>
-        <div className='sticky top-0 bg-black bg-opacity-100  p-2 border-b-2 border-white border-opacity-25 flex justify-between items-center md:justify-around'>
-          <div>
+        <div className='sticky top-0 bg-black bg-opacity-100  p-2  border-opacity-25 flex justify-between items-center md:justify-around'>
+          <div className='flex justify-between py-2 px-6 w-full items-center md:flex md:items-center  md:w-4/5 md:justify-around'>
+            <div className='items-center pr-2 md:pr-0 md:hidden'>
+              <button onClick={onButtonClick}>
+                <AiOutlineAlignLeft className='text-3xl' />
+              </button>
+            </div>
             <div className='p-2 text-right'>
               <div className='relative w-12 h-12'>
-                <img
-                  className='rounded-full shadow-sm cursor-pointer'
-                  src={logo}
-                  onClick={onButtonClick}
-                  alt='logo'
-                />
+                <a href='./'>
+                  <img
+                    className='rounded-full shadow-sm cursor-pointer'
+                    src={logo}
+                    alt='logo'
+                  />
+                </a>
               </div>
             </div>
             <div
-              className='flex flex-col p-2 shadow-2xl  font-semiboldbold absolute bg-black border-2 border-white border-opacity-25 rounded invisible'
+              className='flex flex-col p-2 shadow-2xl mt-40 md:mt-0 font-semiboldbold absolute bg-black border-2 border-white border-opacity-25 rounded invisible md:visible md:static md:flex-row md:border-0 md:space-x-5'
               ref={logoImg}
             >
               <Link
                 to='/'
-                className='font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green-200 to-blue-800 hover:from-green-500 hover:to-yellow-100'
+                className='font-extrabold bg-clip-text text-white hover:text-green-200'
               >
                 Inicio
               </Link>
               <Link
                 to='/skills'
-                className='font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green-200 to-blue-800 hover:from-green-500 hover:to-yellow-100'
+                className='font-extrabold bg-clip-text text-white hover:text-green-200'
               >
                 Habilidades
               </Link>
               <Link
                 to='/projects'
-                className='font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green-200 to-blue-800 hover:from-green-500 hover:to-yellow-100'
+                className='font-extrabold bg-clip-text text-white hover:text-green-200'
               >
                 Proyectos
               </Link>
               <a
                 href='cv/lucaspadularrosa.pdf'
                 target='_blank'
-                className='font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-green-200 to-blue-800 hover:from-green-500 hover:to-yellow-100'
+                className='font-extrabold  bg-clip-text text-white hover:text-green-200'
               >
                 Curriculum
               </a>
             </div>
-          </div>
-          <div className='flex justify-center items-center pr-2 md:pr-0'>
-            <TiCode className='' /> <span className='font-bold'>Lp</span>
           </div>
         </div>
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,20 +5,22 @@ const Home = () => {
   return (
     <>
       <Text name='Sobre mi' />
-      <div className='w-4/5 container mx-auto md:w-2/4 font-bold'>
-        Hola, soy Lucas Padularrosa. Tengo 23 años, actualmente vivo en Rosario,
-        Sante Fe. <br /> A principios del año 2020 comencé a estudiar Análisis
-        de Sistemas en el Instituto Politécnico de Rosario. <br /> Me apasiona
-        el desarrollo de Software, mi primer lenguaje de programación fue “C”,
-        con el cual aprendí las bases de la programación estructurada, lo que
-        fue una ayuda para aprender otros paradigmas de programación, como el
-        funcional y el orientado a objetos. <br /> Este año empecé un bootcamp
-        de programación fullstack en Nucba. El desarrollo web hoy en día es mi
-        prioridad, un gran porcentaje de mis proyectos están hechos en
-        JavaScript. <br /> Mi objetivo es dominar las tecnologías del frontend,
-        aprender la lógica del backend y poder administrar bases de datos. Para
-        poder desarrollarme como programador fullstack. <br /> <br /> Gracias
-        por visitar mi portfolio.
+      <div className='w-3/4 container mx-auto md:w-2/4 font-semibold'>
+        <p className='leading-relaxed text-xl'>
+          Hola, soy Lucas Padularrosa. Tengo 23 años, actualmente vivo en
+          Rosario, Sante Fe. <br /> A principios del año 2020 comencé a estudiar
+          Análisis de Sistemas en el Instituto Politécnico de Rosario. <br /> Me
+          apasiona el desarrollo de Software, mi primer lenguaje de programación
+          fue “C”, con el cual aprendí las bases de la programación
+          estructurada, lo que fue una ayuda para aprender otros paradigmas de
+          programación, como el funcional y el orientado a objetos. <br /> Este
+          año empecé un bootcamp de programación fullstack en Nucba. El
+          desarrollo web hoy en día es mi prioridad, un gran porcentaje de mis
+          proyectos están hechos en JavaScript. <br /> Mi objetivo es dominar
+          las tecnologías del frontend, aprender la lógica del backend y poder
+          administrar bases de datos. Para poder desarrollarme como programador
+          fullstack. <br /> <br /> Gracias por visitar mi portfolio.
+        </p>
       </div>
       <div className='flex justify-center items-center my-16'>
         <button className='border-2 p-2 border-blue-600 rounded hover:border-white'>
